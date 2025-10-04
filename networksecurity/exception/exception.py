cb3e729b28beed6d23479babd782d5912f2196e1
akashgaikwad28 <akash.gaikwad@gmail.com>
2025-10-04 17:01:40 +0530

    correct the custom exception

--- networksecurity/exception/exception.py
+++ networksecurity/exception/exception.py
@@ -1,17 +1,33 @@
 import sys
 from networksecurity.logging import logger
 
+
 class NetworkSecurityException(Exception):
-    def __init__(self,error_message,error_details:sys):
+    def __init__(self, error_message: str, error_details: sys, error_trace=None):
+        super().__init__(error_message)
         self.error_message = error_message
-        _,_,exc_tb = error_details.exc_info()
-        
-        self.lineno=exc_tb.tb_lineno
-        self.file_name=exc_tb.tb_frame.f_code.co_filename 
-    
+        self.error_details = error_details
+
+        # Use provided traceback or fallback to sys.exc_info()
+        exc_type, exc_obj, exc_tb = error_trace if error_trace else sys.exc_info()
+
+        if exc_tb:
+            self.script_name = exc_tb.tb_frame.f_code.co_filename
+            self.lineno = exc_tb.tb_lineno
+        else:
+            # Fallback to caller frame
+            try:
+                import inspect
+                caller_frame = inspect.stack()[2]
+                self.script_name = caller_frame.filename
+                self.lineno = caller_frame.lineno
+            except Exception:
+                self.script_name = "Unknown"
+                self.lineno = "Unknown"
+
     def __str__(self):
-        return "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
-        self.file_name, self.lineno, str(self.error_message))
+        return f"Error occured in python script name [{self.script_name}] line number [{self.lineno}] error message [{self.error_message}]"
+
         
 if __name__=='__main__':
     try:
@@ -19,4 +35,4 @@ if __name__=='__main__':
         a=1/0
         print("This will not be printed",a)
     except Exception as e:
-           raise NetworkSecurityException(e,sys)
+           raise NetworkSecurityException(str(e), sys, sys.exc_info())
